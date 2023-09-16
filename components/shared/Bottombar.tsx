a895d68020a2a34eb975bbe58b39f940df917972
misterchief53 <soderol3@hotmail.com>
2023-09-16 13:50:46 -0600

    made progress on main page

---
+++ components/shared/Bottombar.tsx
@@ -0,0 +1,13 @@
+function Bottombar(){
+    return(
+        <section className={"bottombar"}>
+            <div className={"bottombar_container"}>
+                {sidebarLinks.map((link) => {
+
+                })}
+            </div>
+        </section>
+    )
+}
+
+export default Bottombar
